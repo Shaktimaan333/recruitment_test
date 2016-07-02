fd0e97ff7447acf418dd3e25efebdac64cb0d85f
Bhuvan Agrawal <bhuvan002@gmail.com>
2016-07-02 14:47:38 +0530

    Add sheets to active admin

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -2,4 +2,7 @@ class Attempt < ActiveRecord::Base
   has_many :sheets
   belongs_to :user
   belongs_to :exam
+  def display_name
+    "#{User.find_by(id: self.user_id).name}, #{self.freq}"
+  end
 end
